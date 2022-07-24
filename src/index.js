0018c40bf7bad226fc7ca2cfc84ba1d2c6135ead
Martinpouh <martinchege2015@gmail.com>
2022-07-24 16:12:03 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+const NavBar = () => {
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <Link to="/" style={linkStyles} activeStyle={{ background: "darkblue" }}>
+        Home
+      </Link>
+      <Link to="/about" style={linkStyles}>
+        About
+      </Link>
+      <Link to="/login" style={linkStyles}>
+        Login
+      </Link>
+    </div>
+  );
+};
 
 function Home() {
   return (
@@ -9,8 +36,45 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+const NoPage = () => <p>404 Page not found</p>;
+
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes path="/">
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
